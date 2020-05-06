12fd996c4a28d85568b8432616b31bc7d1314973
alexfac <alexandre.sarrazin@ens.uvsq.fr>
2020-05-06 15:39:32 +0200

    First commit

---
+++ src/main/java/uvsq21603110/App.java
@@ -0,0 +1,8 @@
+package uvsq21603110;
+
+public class App 
+{
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/test/java/uvsq21603110/AppTest.java
@@ -0,0 +1,14 @@
+package uvsq21603110;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void test(){
+
+    }
+}
